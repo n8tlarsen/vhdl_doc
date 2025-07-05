bf5c2be48988fe4f55f20b7dab4b17e4867cd411
Nathan <n8tlarsen@gmail.com>
2025-07-05 17:08:42 -0500

    Update project name, use image instead of femtovg

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use clap::Parser;
-use std::{env, path::PathBuf};
+use std::{env, fs, path::PathBuf};
+
+mod symbol;
+use symbol::make_symbol;
 
 fn default_path(p: &str) -> PathBuf {
     let mut path = env::current_dir().unwrap();
@@ -12,11 +15,14 @@ fn default_path(p: &str) -> PathBuf {
 struct Args {
     #[arg(short, long, default_value = default_path(".").into_os_string())]
     source_path: PathBuf,
-    #[arg(short, long, default_value = default_path("docs").into_os_string())]
-    docs_path: PathBuf,
+    #[arg(short, long, default_value = default_path("doc").into_os_string())]
+    doc_path: PathBuf,
 }
 
 fn main() {
     let args = Args::parse();
-    println!("Source: {:?}, Output {:?}", args.source_path, args.docs_path);
+    println!("Source: {:?}, Output {:?}", args.source_path, args.doc_path);
+    fs::create_dir_all(args.doc_path.clone()).unwrap();
+    make_symbol(args.doc_path);
 }
+
---
+++ src/symbol.rs
@@ -0,0 +1,15 @@
+use std::path::PathBuf;
+use image::{RgbImage, Rgb};
+
+pub fn make_symbol (doc_path: PathBuf) {
+    let mut img = RgbImage::new(32, 32);
+    for x in 15..=17 {
+        for y in 8..24 {
+            img.put_pixel(x, y, Rgb([255, 0, 0]));
+            img.put_pixel(y, x, Rgb([255, 0, 0]));
+        }
+    }
+    let mut out_path = doc_path.into_os_string();
+    out_path.push("/test.png");
+    img.save(out_path).unwrap();
+}
